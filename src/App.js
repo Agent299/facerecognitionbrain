f01fc112a5114fd7df8d7526906420e9263ba53c
Agent299 <meghallium@gmail.com>
2021-04-05 06:55:29 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navigation from "./components/Navigation/Navigation";
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      {/*  <logo />*/}
+      {/*  <ImageLinkForm />*/}
+      {/*<FaceRecognition />*/}
+    </div>
+  );
+}
+
+export default App;
